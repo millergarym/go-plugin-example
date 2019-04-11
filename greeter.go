f4ef1df92c63b7023ba5cf02f8d1fa116ec9c3f5
Gary Miller <gm@wx.io>
2019-04-11 19:44:25 +1000

    compile and load

--- greeter.go
+++ greeter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"plugin"
 )
 
@@ -10,51 +11,59 @@ type Greeter interface {
 	Greet()
 }
 
+func build(l string) error {
+	opath := "./" + l + "/" + l + ".so"
+	spath := "./" + l + "/greeter.go"
+	cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", opath, spath)
+	return cmd.Run()
+}
+
 func main() {
 	// determine module to load
-	lang := "english"
-	if len(os.Args) == 2 {
-		lang = os.Args[1]
-	}
-	var mod string
-	switch lang {
-	case "english":
-		mod = "./eng/eng.so"
-	case "chinese":
-		mod = "./chi/chi.so"
- 	case "swedish":
-	        mod = "./swe/swe.so"
-	default:
-		fmt.Println("don't speak that language")
-		os.Exit(1)
+	lang := []string{"eng"}
+	if len(os.Args) >= 2 {
+		lang = os.Args[1:]
 	}
 
-	// load module
-	// 1. open the so file to load the symbols
-	plug, err := plugin.Open(mod)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	plugins := make([]*plugin.Plugin, 0)
+	for _, l := range lang {
+		// load module
+		// 1. open the so file to load the symbols
+		plug, err := plugin.Open("./" + l + "/" + l + ".so")
+		if err != nil {
+			err = build(l)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			plug, err = plugin.Open("./" + l + "/" + l + ".so")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+		plugins = append(plugins, plug)
 	}
 
-	// 2. look up a symbol (an exported function or variable)
-	// in this case, variable Greeter
-	symGreeter, err := plug.Lookup("Greeter")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	for _, plug := range plugins {
+		// 2. look up a symbol (an exported function or variable)
+		// in this case, variable Greeter
+		symGreeter, err := plug.Lookup("Greeter")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// 3. Assert that loaded symbol is of a desired type
+		// in this case interface type Greeter (defined above)
+		var greeter Greeter
+		greeter, ok := symGreeter.(Greeter)
+		if !ok {
+			fmt.Println("unexpected type from module symbol")
+			os.Exit(1)
+		}
+
+		// 4. use the module
+		greeter.Greet()
 	}
-
-	// 3. Assert that loaded symbol is of a desired type
-	// in this case interface type Greeter (defined above)
-	var greeter Greeter
-	greeter, ok := symGreeter.(Greeter)
-	if !ok {
-		fmt.Println("unexpected type from module symbol")
-		os.Exit(1)
-	}
-
-	// 4. use the module
-	greeter.Greet()
-
 }
